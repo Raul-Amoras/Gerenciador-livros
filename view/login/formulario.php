e3cb33796af61273dab622a8ebcae9817b19377e
Raul Amoras <raul.amoras@gmail.com>
2021-09-01 19:45:35 -0300

    add tela login e validacao de senha

---
+++ view/login/formulario.php
@@ -0,0 +1,13 @@
+<?php include __DIR__ . '/../header.php'; ?>
+<form action="/realizar-login" method="post">
+  <div class="form-group">
+    <label for="email">E-mail</label>
+    <input type="email" name="email" class="form-control">
+  </div>
+  <div class="form-group">
+    <label for="senha">Senha</label>
+    <input type="password" name="senha" class="form-control">
+  </div>
+  <button class="btn btn-primary">Entrar</button>
+</form>
+<?php include __DIR__ . '/../footer.php'; ?>
